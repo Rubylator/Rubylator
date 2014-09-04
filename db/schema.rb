78ef5a2c5eaeb3223b3eb4e1d98caaa6a7394cc7
Samuel Mehrbrodt <s.mehrbrodt@gmail.com>
2014-09-04 14:51:18 +0200

    Project administration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140904111521) do
+ActiveRecord::Schema.define(version: 20140904112538) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
